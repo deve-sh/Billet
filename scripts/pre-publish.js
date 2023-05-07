c6791d87d5a628b1ae580d220e5381445fe0d127
Devesh Kumar <devesh2027@gmail.com>
2023-05-07 15:13:42 +0530

    scripts: doc: Add README copying to npm release

--- scripts/pre-publish.js
+++ scripts/pre-publish.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.copyFileSync('./package.json', './dist/package.json');
-fs.writeFileSync('./dist/.npmignore', 'node_modules');
+fs.copyFileSync("./package.json", "./dist/package.json");
+fs.writeFileSync("./dist/.npmignore", "node_modules");
+fs.writeFileSync("./dist/README.md", fs.readFileSync("./README.md", "utf-8"));
