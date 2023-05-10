3eacff9c85cd0be987d5c96e86c9a5522dfeb2f0
Devesh Kumar <devesh2027@gmail.com>
2023-05-10 23:44:58 +0530

    feat: client: console overwriting function generator

--- src/client/logging/overwrite-console-functions.ts
+++ src/client/logging/overwrite-console-functions.ts
@@ -1,38 +1,36 @@
 import type Log from "../types/Log";
+import type LogLevels from "../types/LogLevels";
+
 import createLog from "./create-log";
 
 // Preserve original function references
 let originalFunctions = {
 	log: console.log,
+	info: console.info,
 	error: console.error,
 	warn: console.warn,
 	debug: console.debug,
 };
 
+const generateLoggerFunction =
+	(level: LogLevels, push: (log: Log) => void) =>
+	(...args: (string | unknown)[]) => {
+		try {
+			push(createLog(level)(args));
+			originalFunctions[level](...args);
+		} catch {}
+	};
+
 const overWriteConsoleFunctions = (push: (log: Log) => void) => {
-	console.log = (...args: (string | unknown)[]) => {
-		push(createLog("info")(args));
-		originalFunctions.log(...args);
-	};
-	console.info = (...args: (string | unknown)[]) => {
-		push(createLog("info")(args));
-		originalFunctions.log(...args);
-	};
-	console.error = (...args: (string | unknown)[]) => {
-		push(createLog("error")(args));
-		originalFunctions.error(...args);
-	};
-	console.warn = (...args: (string | unknown)[]) => {
-		push(createLog("warn")(args));
-		originalFunctions.warn(...args);
-	};
-	console.debug = (...args: (string | unknown)[]) => {
-		push(createLog("debug")(args));
-		originalFunctions.debug(...args);
-	};
+	console.log = generateLoggerFunction("info", push);
+	console.info = generateLoggerFunction("info", push);
+	console.error = generateLoggerFunction("error", push);
+	console.warn = generateLoggerFunction("warn", push);
+	console.debug = generateLoggerFunction("debug", push);
 
 	return () => {
 		console.log = originalFunctions.log;
+		console.info = originalFunctions.info;
 		console.error = originalFunctions.error;
 		console.warn = originalFunctions.warn;
 		console.debug = originalFunctions.debug;
