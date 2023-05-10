d68eced5e42490e7d32a42c9733e07ebb13a33bb
Devesh Kumar <devesh2027@gmail.com>
2023-05-10 10:36:11 +0530

    tests: client: Remove mocks folder for now

--- src/client/tests/mocks/create-fetch-mocker.ts
+++
@@ -1,8 +0,0 @@
-const createFetchMocker = (mockVariableToUpdate) => {
-	// @ts-expect-error
-	globalThis.fetch = (endpoint: string, options: unknown) => {
-		mockVariableToUpdate = { endpoint, options };
-	};
-};
-
-export default createFetchMocker;
